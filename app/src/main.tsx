f8d20b1dab269ef78ac0d75f55d2bf5206b85505
Stas <stasbwork2@gmail.com>
2025-10-15 22:14:19 +0300

    revert to single page

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,21 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
 import "./index.css";
 
-import Home from "./pages/Home";
-import Documents from "./pages/Documents";
-import Games from "./pages/Games";
-import DoNothingGamePage from "./pages/games/DoNothingGamePage";
-import NotFound from "./pages/NotFound";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/documents", element: <Documents /> },
-  { path: "/games", element: <Games /> },
-  { path: "/games/do-nothing", element: <DoNothingGamePage /> },
-  { path: "*", element: <NotFound /> },
-]);
-
-createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById("root")!).render(<App />);
