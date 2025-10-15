3cb31dd0946f5329f7353290bdba9b4078abb3f6
Stas <stasbwork2@gmail.com>
2025-10-15 20:55:06 +0300

    fixing sitemap.js

--- deploy/sitemap.js
+++ deploy/sitemap.js
@@ -1,17 +1,22 @@
 function gen(r) {
-  const base = `${r.variables.scheme}://${r.headersIn.host}`;
-  const routes = ["/"];
-  const today = new Date().toISOString().slice(0, 10);
+  var base = r.variables.scheme + "://" + r.headersIn.host;
+  var routes = ["/"];
+  var today = new Date().toISOString().slice(0, 10);
 
-  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
-  for (const p of routes) {
-    const priority = p === "/" ? "1.0" : "0.7";
-    xml += `  <url><loc>${base}${p}</loc><lastmod>${today}</lastmod><changefreq>daily</changefreq><priority>${priority}</priority></url>\n`;
+  var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+  for (var i = 0; i < routes.length; i++) {
+    var p = routes[i];
+    var priority = (p === "/") ? "1.0" : "0.7";
+    xml += '  <url><loc>' + base + p + '</loc><lastmod>' + today +
+           '</lastmod><changefreq>daily</changefreq><priority>' + priority +
+           '</priority></url>\n';
   }
-  xml += `</urlset>\n`;
 
-  r.headersOut["Content-Type"] = "application/xml; charset=utf-8";
+  xml += '</urlset>\n';
+
+  r.headersOut['Content-Type'] = 'application/xml; charset=utf-8';
   r.return(200, xml);
 }
 
