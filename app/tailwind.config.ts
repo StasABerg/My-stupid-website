a26a39e9c6dece47279b372fc8cc6c5ba6ba1cc6
Stas <stasbwork2@gmail.com>
2025-10-27 22:03:07 +0200

    aaaaa

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -13,7 +13,6 @@ const terminalColors = {
 
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
