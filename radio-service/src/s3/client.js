c2ef70254414f61b363b3cf0434048c138601804
Stas <stasbwork2@gmail.com>
2025-11-03 00:02:58 +0200

    mlem

--- radio-service/src/s3/client.js
+++ radio-service/src/s3/client.js
@@ -1,5 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { SignatureV4 } from "@smithy/signature-v4";
+import { createHash } from "node:crypto";
 import { config } from "../config/index.js";
 
 const credentials = {
@@ -7,11 +8,30 @@ const credentials = {
   secretAccessKey: config.s3.secretAccessKey,
 };
 
+class NodeSha256 {
+  constructor() {
+    this.hash = createHash("sha256");
+  }
+
+  update(data) {
+    if (typeof data === "string") {
+      this.hash.update(data, "utf8");
+    } else {
+      this.hash.update(data);
+    }
+  }
+
+  async digest() {
+    return this.hash.digest();
+  }
+}
+
 function createSigner() {
   return new SignatureV4({
     credentials,
     region: config.s3.signingRegion ?? config.s3.region,
     service: config.s3.signingService ?? "s3",
+    sha256: NodeSha256,
   });
 }
 
