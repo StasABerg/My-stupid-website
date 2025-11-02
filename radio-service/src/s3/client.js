63535c837d7b7dfd6830aaa798d8ea62908d214e
Stas <stasbwork2@gmail.com>
2025-11-03 01:20:03 +0200

    testing....

--- radio-service/src/s3/client.js
+++ radio-service/src/s3/client.js
@@ -1,17 +1,27 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import { SignatureV4 } from "@smithy/signature-v4";
+import { Sha256 } from "@aws-crypto/sha256-js";
 import { config } from "../config/index.js";
 import { logger } from "../logger.js";
+
+const credentials = {
+  accessKeyId: config.s3.accessKeyId,
+  secretAccessKey: config.s3.secretAccessKey,
+};
+
+const signer = new SignatureV4({
+  credentials,
+  region: config.s3.signingRegion ?? config.s3.region,
+  service: config.s3.signingService ?? "garage",
+  sha256: Sha256,
+});
 
 const s3Client = new S3Client({
   region: config.s3.region,
   endpoint: config.s3.endpoint,
   forcePathStyle: true,
-  credentials: {
-    accessKeyId: config.s3.accessKeyId,
-    secretAccessKey: config.s3.secretAccessKey,
-  },
-  signingRegion: config.s3.signingRegion ?? config.s3.region,
-  signingName: config.s3.signingService ?? "s3",
+  credentials,
+  signer,
 });
 
 s3Client.middlewareStack.add(
