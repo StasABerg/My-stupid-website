8de08b4e54d1c7774457223d619d0ccbbeb83464
Stas <stasbwork2@gmail.com>
2025-11-16 20:48:17 +0200

    mlem

--- app/public/sw.js
+++ app/public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "gitgud-radio-shell-v1";
-const APP_SHELL = ["/", "/radio", "/favicon.ico", "/manifest.webmanifest"];
+const CACHE_NAME = "gitgud-radio-shell-v2";
+const APP_SHELL = ["/", "/radio", "/favicon.ico"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
